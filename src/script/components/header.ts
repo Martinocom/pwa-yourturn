ed5fbe92881dd907b4743c62229a4c4648edef87
Marcin Pabich <martinoqom@gmail.com>
2022-03-06 14:43:05 +0100

    Cow logout

--- src/script/components/header.ts
+++ src/script/components/header.ts
@@ -1,5 +1,6 @@
 import { LitElement, css, html } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 @customElement('app-header')
 export class AppHeader extends LitElement {
@@ -10,12 +11,19 @@ export class AppHeader extends LitElement {
     return css`
       header {
         display: flex;
-        justify-content: flex-start;
+        justify-content: space-between;
         align-items: center;
         background: var(--app-color-primary);
         height: 3em;
         padding: 0px 16px;
         box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05), 0 2px 5px 0 rgba(0, 0, 0, 0.1);
+        outline: none;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
       }
 
       header h1 {
@@ -31,6 +39,28 @@ export class AppHeader extends LitElement {
         font-weght: normal;
         margin-left: 15px;
         color: white;
+      }
+
+      .grouped {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+      }
+
+      #logout {
+        width: 48px;
+        height: 38px;
+        font-size: 1.5em;
+        outline: none;
+        line-height: 38px;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        cursor: pointer;
       }
 
       @media only screen and (min-width: 724px) {
@@ -54,17 +84,27 @@ export class AppHeader extends LitElement {
     super();
   }
 
+  private async onLogoutClick() {
+    getAuth()?.signOut()
+  }
+
   render() {
     if (this.version == null || this.version == "") {
       return html`
         <header>
             <h1>${this.title}</h1>
+            <div id="logout" @click="${this.onLogoutClick}">🐄</div>
         </header>
       `;
     } else {
       return html`
         <header>
-            <h1>${this.title} <h5>(${this.version})</h5></h1>
+            <div class="grouped">
+              <h1>${this.title}</h1>
+              <h5>(${this.version})</h5>
+            </div>
+
+            <div id="logout" @click="${this.onLogoutClick}">🐄</div>
         </header>
       `;
     }
