27c29e87ed1f4215879360a4fb5fe9e24d97563f
Marcin Pabich <martinoqom@gmail.com>
2022-08-30 11:40:15 +0200

    Trying restyle

--- src/script/components/my-activity.ts
+++ src/script/components/my-activity.ts
@@ -2,6 +2,8 @@ import { LitElement, html } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 import { Activity } from '../model/activity';
 import { TimeConverter } from '../utils/time-converter';
+import { defaultStyle } from './styles/default.style';
+import { differentCardStyle } from './styles/different-card.style';
 import { modernCardStyle } from './styles/modern-card.style';
 
 @customElement('my-activity')
@@ -13,6 +15,12 @@ export class MyActivity extends LitElement {
     @property({ type: Activity })
     activity!: Activity;
 
+    @property({ type: String })
+    currentUser!: string;
+
+    @property({ type: Number })
+    order!: number;
+
     private image = new Image()
 
     constructor() {
@@ -22,37 +30,42 @@ export class MyActivity extends LitElement {
     async firstUpdated() {
         this.image.id = "image";
         this.image.src = this.activity.image
-        const cardRef = this.shadowRoot?.getElementById('card')
+        /*const cardRef = this.shadowRoot?.getElementById('card')
         const smallBtnRef = this.shadowRoot?.getElementById('btn-small');
         const bigBtnRef = this.shadowRoot?.getElementById('btn-big');
+        const contentAllRef = this.shadowRoot?.getElementById('content-counters');
 
         if (cardRef) {
             setTimeout(() => {
-                if (cardRef.offsetWidth <= 325) {
+                if (cardRef.offsetWidth <= 310) {
                     smallBtnRef?.classList?.remove("hidden")
                     bigBtnRef?.classList?.add("hidden")
+                    contentAllRef?.classList.add('centered')
                 } else {
                     smallBtnRef?.classList?.add("hidden")
                     bigBtnRef?.classList?.remove("hidden")
+                    contentAllRef?.classList.remove('centered')
                 }
             }, 100);
 
 
             window.addEventListener('resize', (_) => {
                 console.log(cardRef.offsetWidth)
-                if (cardRef.offsetWidth <= 325) {
+                if (cardRef.offsetWidth <= 310) {
                     smallBtnRef?.classList?.remove("hidden")
                     bigBtnRef?.classList?.add("hidden")
+                    contentAllRef?.classList.add('centered')
                 } else {
                     smallBtnRef?.classList?.add("hidden")
                     bigBtnRef?.classList?.remove("hidden")
+                    contentAllRef?.classList.remove('centered')
                 }
             })
-        }
+        }*/
 
     }
 
-    async switchContent(elementName: string) {
+    /*async switchContent(elementName: string) {
         const element = this.shadowRoot?.getElementById(elementName)
         if (element != null) {
             if (element.classList.contains("name")) {
@@ -65,55 +78,145 @@ export class MyActivity extends LitElement {
                 element.innerHTML = elementName.indexOf("marcin") >= 0 ? "Marcin" : "Marta"
             }
         }
+    }*/
+
+    isRedEnabled(name: string) {
+        return this.activity.nextTurnName == name && this.activity.nextTurnName == this.currentUser;
     }
+
+    /*getSvgFromTitle(): string {
+        var path = 'assets/icons/check.svg'
+        if (this.activity.title == 'Cucinare') {
+            path = 'assets/icons/kitchen.svg'
+        } else if (this.activity.title == 'Cucina Pulizie') {
+            path = 'assets/icons/clean_kitchen.svg'
+        } else if (this.activity.title == 'Lavastoviglie') {
+            path = 'assets/icons/dishwasher.svg'
+        } else if (this.activity.title == 'Bagno Grande') {
+            path = 'assets/icons/bathroom.svg'
+        } else if (this.activity.title == 'Rifiuti') {
+            path = 'assets/icons/rubbish.svg'
+        }
+        return path;
+    }*/
 
     static get styles() {
         //return defaultStyle();
         return modernCardStyle();
+        //return differentCardStyle();
     }
 
 
-    render() {
+    render() {/*
+        return html`
+        <div class='card'>
+            <div class='photo'>
+                <img id="image" src='${this.activity.image}'/>
+            </div>
+
+            <div class='header'>
+                <div class="activity-icon-container">
+                    <img class="activity-icon" src="${this.getSvgFromTitle()}" />
+                </div>
+
+                <div class="title-container">
+                    <h1>${this.activity.title}</h1>
+                    <div class="subtitle-container">
+                        <h2>${this.activity.lastCheck.name} - ${TimeConverter.fromEpoch(this.activity.lastCheck.date, 'both')}</h2>
+                    </div>
+                </div>
+
+                <div class="actions-container">
+                    <div class="action-container" @click="${() => { this.dispatchEvent(new CustomEvent('take-photo', { detail: { id: this.id }})) }}">
+                        <img class="action-photo" src="assets/icons/camera-32.png" />
+                    </div>
+                </div>
+            </div>
+
+            <div class='bottom'>
+                <div class="person-counter">
+                    <div class='badge ${this.isRedEnabled('Marcin') ? 'red' : ''}'>${this.isRedEnabled('Marcin') ? '!' : ''}</div>
+                    <div>Marcin ${this.activity.checksMarcin.length}</div>
+                </div>
+                <div class="person-counter">
+                    <div class='badge ${this.isRedEnabled('Marta') ? 'red' : ''}'>${this.isRedEnabled('Marta') ? '!' : ''}</div>
+                    <div>Marta ${this.activity.checksMarta.length}</div>
+                </div>
+            </div>
+        </div>
+        `
+        */
         return html `
             <div class="card" id="card">
                 <div class="image-container">
                     <img id="image" src='${this.activity.image}'/>
 
                     <div class="badge-container">
-                        <div class="badge">
-                            ${this.activity.lastCheck.name}
-                        </div>
-
-                        <div class="badge">
-                            ${TimeConverter.fromEpoch(this.activity.lastCheck.date, 'both')}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="content">
-                    <div class="content-details">
-                        <div class="content-title">
+                        <!--<div class="badge">
                             <h1>${this.activity.title}</h1>
                             <div id="btn-small" class="clickable small hidden" @click="${() => { this.dispatchEvent(new CustomEvent('take-photo', { detail: { id: this.id }})) }}">
                                 <img class="camera" src="assets/icons/camera-32.png" />
                             </div>
-                        </div>
-                        <div class="content-badge-container">
-                            <div class="badge">
-                                <div class="badge-number ${this.activity.nextTurnName == "Marcin" ? "red" : ""}">${this.activity.checksMarcin.length}</div>
-                                <div class="badge-text ${this.activity.nextTurnName == "Marcin" ? "red" : ""}">Marcin</div>
-                            </div>
-                            <div class="badge">
-                                <div class="badge-number  ${this.activity.nextTurnName == "Marta" ? "red" : ""}">${this.activity.checksMarta.length}</div>
-                                <div class="badge-text ${this.activity.nextTurnName == "Marta" ? "red" : ""}">Marta</div>
-                            </div>
-                        </div>
-                    </div>
+                        </div>-->
 
-                    <div id="btn-big" class="clickable big hidden" @click="${() => { this.dispatchEvent(new CustomEvent('take-photo', { detail: { id: this.id }})) }}">
-                        <img class="camera" src="assets/icons/camera-32.png" />
+                        <div class="badge">
+                            <div class='badge-icon ${this.isRedEnabled('Marcin') ? 'red' : ''}'>${this.activity.checksMarcin.length}</div>
+                            <div class='badge-content'>Marcin</div>
+                        </div>
+
+                        <div class="badge">
+                            <div class='badge-icon ${this.isRedEnabled('Marta') ? 'red' : ''}'>${this.activity.checksMarta.length}</div>
+                            <div class='badge-content'>Marta</div>
+                        </div>
                     </div>
                 </div>
+
+                <div class='content'>
+                    <div class="title">
+                        <h1>${this.activity.title}</h1>
+                        <h2>${this.activity.lastCheck.name} - ${TimeConverter.fromEpoch(this.activity.lastCheck.date, 'both')}</h2>
+                    </div>
+
+                    <div class="actions">
+                        <div class="action" @click="${() => { this.dispatchEvent(new CustomEvent('take-photo', { detail: { id: this.id }})) }}">
+                            <img class="action-photo" src="assets/icons/camera-32.png" />
+                        </div>
+                    </div>
+                </div>
+
+                <!--
+                <div class="content">
+                    <div class="content-details">
+                        <div class="content-all">
+                            <div class="content-counters" id='content-counters'>
+                                <div class="counter-container ${this.isRedEnabled('Marcin') ? 'red' : ''}">
+                                    <div class="counter text">Marcin</div>
+                                    <div class="counter number">${this.activity.checksMarcin.length}</div>
+                                </div>
+
+                                <div class="counter-container ${this.isRedEnabled('Marta') ? 'red' : ''}">
+                                    <div class="counter text">Marta</div>
+                                    <div class="counter number">${this.activity.checksMarta.length}</div>
+                                </div>
+                                <!-
+                                <div class="badge">
+                                    <div class="badge-number ${this.activity.nextTurnName == "Marcin" ? "red" : ""}">${this.activity.checksMarcin.length}</div>
+                                    <div class="badge-text ${this.activity.nextTurnName == "Marcin" ? "red" : ""}">Marcin</div>
+                                </div>
+                                <div class="badge">
+                                    <div class="badge-number  ${this.activity.nextTurnName == "Marta" ? "red" : ""}">${this.activity.checksMarta.length}</div>
+                                    <div class="badge-text ${this.activity.nextTurnName == "Marta" ? "red" : ""}">Marta</div>
+                                </div>--
+                            </div>
+
+
+                            <div id="btn-big" class="clickable big hidden" @click="${() => { this.dispatchEvent(new CustomEvent('take-photo', { detail: { id: this.id }})) }}">
+                                <img class="camera" src="assets/icons/camera-32.png" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>-->
             </div>
         `;
         /*return html`
--- src/script/components/styles/modern-card.style.ts
+++ src/script/components/styles/modern-card.style.ts
@@ -1,6 +1,142 @@
 import { css } from 'lit';
 
+const buttonSize = css`2.5em`;
+const photoSize = css`1.4em`;
+
 export function modernCardStyle() {
+
+return css`
+.card {
+    border-radius: var(--app-border-radius-big);
+    box-shadow: var(--app-shadow);
+    min-width: var(--app-min-card-width);
+    background: var(--app-color-white);
+    overflow: hidden;
+    box-sizing: border-box;
+    outline: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+.image-container {
+    position: relative;
+    width: 100%;
+}
+
+#image {
+    z-index: -1;
+    object-fit: cover;
+    width: 100%;
+    max-height: 7em;
+    margin: 0;
+}
+
+.badge-container {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    align-content: center;
+    gap: 1em;
+    bottom: 1em;
+    left: 1em;
+}
+
+.badge {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    color: white;
+    border-radius: 0.8em;
+    font-size: 0.85em;
+    background: #1E2019BB;
+}
+
+.badge-icon {
+    background: var(--app-color-green);
+    border-radius: 0.8em;
+    height: 100%;
+    text-align: center;
+    font-weight: bold;
+    color: white;
+    padding: 0.4em;
+    margin-right: 0.6em;
+    width: 2em;
+    font-size: 1em;
+}
+
+.badge-icon.red {
+    background: var(--app-color-red);
+}
+
+.badge-content {
+    margin-right: 1em;
+}
+
+.content {
+    display: flex;
+    flex-directon: row;
+    margin-top: 0.8em;
+    margin-bottom: 1em;
+    margin-left: 1.2em;
+    margin-right: 1.2em;
+    align-content: center;
+    align-items: center;
+}
+
+h1, h2 {
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    font-size: 1.1em;
+    font-weight: 600;
+}
+
+h2 {
+    font-size: 0.9em;
+    font-weight: 300;
+}
+
+.actions {
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    justify-content: flex-end;
+}
+
+.action {
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100%;
+    box-sizing: border-box;
+    background: var(--app-color-primary);
+    max-width: ${buttonSize};
+    max-height: ${buttonSize};
+    width: ${buttonSize};
+    height: ${buttonSize};
+}
+
+.action-photo {
+    max-width: ${photoSize};
+    max-height: ${photoSize};
+    width: ${photoSize};
+    height: ${photoSize};
+}
+
+`
+
+/*
     return css`
 .card {
     border-radius: var(--app-border-radius-big);
@@ -39,8 +175,8 @@ export function modernCardStyle() {
     align-items: center;
     align-content: center;
     gap: 0.5em;
-    top: 0.5em;
-    right: 0.5em;
+    bottom: 1em;
+    left: 0.5em;
 }
 
 .content {
@@ -54,6 +190,14 @@ export function modernCardStyle() {
 
 .content-details {
     flex-grow: 1;
+}
+
+.content-all {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 0.5em;
 }
 
 .content-badge-container {
@@ -72,13 +216,21 @@ export function modernCardStyle() {
     justify-content: space-between;
     align-items: center;
     align-content: center;
+
+    /*position: absolute;
+    bottom: 0em;
+    margin: 1em;
+    color: white;
+    /*border-radius: 0.8em;
+    padding: 0.4em 0.8em 0.4em 0.8em;
+    background: #1E2019BB;
 }
 
 h1 {
     margin: 0;
     padding: 0;
-    font-size: 1.2em;
-    line-height: 1.2em;
+    font-size: 1.4em;
+    line-height: 1.4em;
     color: --var(--app-color-black);
 }
 
@@ -89,10 +241,10 @@ h1 {
     align-items: center;
     align-content: center;
     color: white;
-    border-radius: 2em;
+    border-radius: 0.8em;
     font-size: 0.75em;
     padding: 0.4em 0.8em 0.4em 0.8em;
-    background: #1E201999;
+    background: #1E2019BB;
 }
 
 .badge > * {
@@ -103,10 +255,11 @@ h1 {
     font-size: 1em;
 }
 
+/*
 .badge-number {
-    width: 3em;
+    width: 2em;
     background: var(--app-color-primary);
-    border-radius: 2em;
+    border-radius: 0.8em;
     border: 1px solid var(--app-color-primary);
     z-index: 5;
 }
@@ -122,22 +275,66 @@ h1 {
     font-size: 1em;
     padding-left: 2em;
     padding-right: 1.6em;
-    border-radius: 0 2em 2em 0;
-    margin-left: -1em;
+    //border-radius: 0 0.4em 0.4em 0;
+    margin-left: -1.5em;
     color: var(--app-color-black);
-    border: 1px solid var(--app-color-primary);
+    //border: 1px solid var(--app-color-primary);
 }
 
 .badge-text.red {
-    border: 1px solid var(--app-color-red);
+    //border: 1px solid var(--app-color-red);
 }
 
-.content-badge-container .badge {
-    background: transparent;
+.content-counters {
+    display: flex;
+    flex-flow: row;
+    align-items: center;
     margin: 0;
     padding: 0;
     overflow: hidden;
     box-sizing: border-box;
+    gap: 1em;
+}
+
+.content-counters.centered {
+    justify-content: center;
+    flex: 1;
+}
+
+.content-counters.centered > * {
+    flex: 1;
+
+}
+
+.counter-container {
+    background: rgb(215 230 238);
+    border-radius: 1em;
+    padding: 0.8em;
+    padding-bottom: 0.6em;
+    min-width: 4.5em;
+    max-width: 5.5em;
+}
+
+.counter-container.red {
+    background: rgb(255 206 206);
+}
+
+.counter {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+.counter.text {
+    font-size: 0.8em;
+    color: var(--app-color-black);
+    margin-bottom: 0.2em;
+}
+
+.counter.number {
+    font-size: 1.3em;
+    color: var(--app-color-black);
+    font-weight: bold;
 }
 
 .clickable {
@@ -149,7 +346,7 @@ h1 {
     background: var(--app-color-primary);
     flex-grow: 0;
     border-radius: 2em;
-    width: 5em;
+    width: 4.5em;
     height: 1.5em;
 }
 
@@ -167,4 +364,4 @@ h1 {
     transform: scale(0.55);
 }
 `
-}
+}*/
